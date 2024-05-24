7092eace56fa38b74ec1730e958abe7561ab6304
jose55440 <119807251+jose55440@users.noreply.github.com>
2024-05-24 10:58:47 +0200

    .

---
+++ views/createNewEquipamiento.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creacion de equipacion</title>
+</head>
+<body>
+    <form action="" method="get">
+        <label for="nombre">nombre</label>
+        <input type="text" name="nombre" id="nombre">
+        <br>
+        <label for="numSerie">Numero de Serie</label>
+        input:te
+    </form>
+</body>
+</html>
